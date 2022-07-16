e6a58fbe4785f21a3f051c1a55fecc3a6d05cfbc
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2022-07-16 14:27:20 +0200

    Add background parameter to fg

--- src/Output.php
+++ src/Output.php
@@ -9,21 +9,38 @@ class Output
     protected mixed $stream;
     protected array $fg = [
         'black' => [0, 30],
-        'darkgray' => [1, 30],
-        'blue' => [0, 34],
-        'lightblue' => [1, 34],
-        'green' => [0, 32],
-        'lightgreen' => [1, 32],
-        'cyan' => [0, 36],
-        'lightcyan' => [1, 36],
+        'gray' => [1, 30],
+        'grey' => [1, 30],
         'red' => [0, 31],
         'lightred' => [1, 31],
-        'purple' => [0, 35],
-        'lightpurple' => [1, 35],
+        'green' => [0, 32],
+        'lightgreen' => [1, 32],
         'brown' => [0, 33],
         'yellow' => [1, 33],
+        'blue' => [0, 34],
+        'lightblue' => [1, 34],
+        'purple' => [0, 35],
+        'lightpurple' => [1, 35],
+        'magenta' => [0, 35],
+        'lightmagenta' => [1, 35],
+        'cyan' => [0, 36],
+        'lightcyan' => [1, 36],
         'lightgray' => [0, 37],
+        'lightgrey' => [0, 37],
         'white' => [1, 37],
+    ];
+    protected array $bg = [
+        'black' => 40,
+        'red' => 41,
+        'green' => 42,
+        'yellow' => 43,
+        'blue' => 44,
+        'purple' => 45,
+        'magenta' => 45,
+        'cyan' => 46,
+        'gray' => 47,
+        'grey' => 47,
+        'white' => 47,
     ];
 
     public function __construct(protected readonly string $target)
@@ -45,9 +62,14 @@ class Output
         fflush($this->stream);
     }
 
-    public function fg(string $text, string $color): string
+    public function fg(string $text, string $color, string $background = null): string
     {
         [$first, $second] = $this->fg[$color];
+
+        if ($background) {
+            $bg = $this->bg[$background];
+            return "\033[$first;${second};${bg}m$text\033[0m";
+        }
 
         return "\033[$first;${second}m$text\033[0m";
     }
--- tests/OutputTest.php
+++ tests/OutputTest.php
@@ -9,7 +9,8 @@ test('Foreground colors', function () {
     $output = new Output('php://output');
 
     expect($output->fg('test', 'black'))->toBe("\033[0;30mtest\033[0m");
-    expect($output->fg('test', 'darkgray'))->toBe("\033[1;30mtest\033[0m");
+    expect($output->fg('test', 'gray'))->toBe("\033[1;30mtest\033[0m");
+    expect($output->fg('test', 'grey'))->toBe("\033[1;30mtest\033[0m");
     expect($output->fg('test', 'red'))->toBe("\033[0;31mtest\033[0m");
     expect($output->fg('test', 'lightred'))->toBe("\033[1;31mtest\033[0m");
     expect($output->fg('test', 'green'))->toBe("\033[0;32mtest\033[0m");
@@ -18,10 +19,30 @@ test('Foreground colors', function () {
     expect($output->fg('test', 'yellow'))->toBe("\033[1;33mtest\033[0m");
     expect($output->fg('test', 'blue'))->toBe("\033[0;34mtest\033[0m");
     expect($output->fg('test', 'lightblue'))->toBe("\033[1;34mtest\033[0m");
+    expect($output->fg('test', 'magenta'))->toBe("\033[0;35mtest\033[0m");
+    expect($output->fg('test', 'lightmagenta'))->toBe("\033[1;35mtest\033[0m");
     expect($output->fg('test', 'purple'))->toBe("\033[0;35mtest\033[0m");
     expect($output->fg('test', 'lightpurple'))->toBe("\033[1;35mtest\033[0m");
     expect($output->fg('test', 'cyan'))->toBe("\033[0;36mtest\033[0m");
     expect($output->fg('test', 'lightcyan'))->toBe("\033[1;36mtest\033[0m");
     expect($output->fg('test', 'lightgray'))->toBe("\033[0;37mtest\033[0m");
+    expect($output->fg('test', 'lightgrey'))->toBe("\033[0;37mtest\033[0m");
     expect($output->fg('test', 'white'))->toBe("\033[1;37mtest\033[0m");
+});
+
+
+test('Background colors', function () {
+    $output = new Output('php://output');
+
+    expect($output->fg('test', 'lightgrey', 'black'))->toBe("\033[0;37;40mtest\033[0m");
+    expect($output->fg('test', 'white', 'red'))->toBe("\033[1;37;41mtest\033[0m");
+    expect($output->fg('test', 'lightgreen', 'green'))->toBe("\033[1;32;42mtest\033[0m");
+    expect($output->fg('test', 'yellow', 'yellow'))->toBe("\033[1;33;43mtest\033[0m");
+    expect($output->fg('test', 'blue', 'blue'))->toBe("\033[0;34;44mtest\033[0m");
+    expect($output->fg('test', 'lightpurple', 'purple'))->toBe("\033[1;35;45mtest\033[0m");
+    expect($output->fg('test', 'purple', 'magenta'))->toBe("\033[0;35;45mtest\033[0m");
+    expect($output->fg('test', 'cyan', 'cyan'))->toBe("\033[0;36;46mtest\033[0m");
+    expect($output->fg('test', 'white', 'white'))->toBe("\033[1;37;47mtest\033[0m");
+    expect($output->fg('test', 'white', 'gray'))->toBe("\033[1;37;47mtest\033[0m");
+    expect($output->fg('test', 'white', 'grey'))->toBe("\033[1;37;47mtest\033[0m");
 });
