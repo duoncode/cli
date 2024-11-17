3b47a7c8c5c483988ac2646c454a71efb833b2e0
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2024-11-17 11:12:05 +0100

    Add and use fiveorbs/dev

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,33 +1,11 @@
 <?php
 
-$finder = PhpCsFixer\Finder::create();
-$config = new PhpCsFixer\Config();
+declare(strict_types=1);
 
+use PhpCsFixer\Finder;
+use FiveOrbs\Development\PhpCsFixer\Config;
 
-return $config->setRules([
-    '@PSR12' => true,
-    '@PhpCsFixer' => true,
-    'no_extra_blank_lines' => false,
-    'multiline_whitespace_before_semicolons' => false,
-    'no_whitespace_before_comma_in_array' => true,
-    'trailing_comma_in_multiline' => true,
-    'braces' => true,
-    'array_indentation' => true,
-    'array_syntax' => ['syntax' => 'short'],
-    'class_definition' => ['space_before_parenthesis' => true],
-    'method_argument_space' => ['on_multiline' => 'ensure_fully_multiline'],
-    'multiline_comment_opening_closing' => false,
-    'no_unused_imports' => false,
-    'phpdoc_to_comment' => false,
-    'yoda_style' => false,
-    'increment_style' => ['style' => 'post'],
-    'cast_spaces' => ['space' => 'none'],
-    'concat_space' => [
-        'spacing' => 'one',
-    ],
-    'global_namespace_import' => [
-        'import_classes' => true,
-        'import_constants' => null,
-        'import_functions' => null,
-    ],
-])->setFinder($finder);
+$finder = Finder::create()->in([__DIR__ . '/src', __DIR__ . '/tests']);
+$config = new Config();
+
+return $config->setFinder($finder);
